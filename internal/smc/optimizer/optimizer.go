fefdf05fe3bcc8b23cdc439920d403fa220d867c
Geison Biazus <geisonbiazus@gmail.com>
2018-10-02 09:25:45 -0300

    Optimize a simple FSM

--- internal/smc/optimizer/optimizer.go
+++ internal/smc/optimizer/optimizer.go
@@ -1,1 +1,34 @@
 package optimizer
+
+import "github.com/geisonbiazus/smc/internal/smc/semantic"
+
+type Optimizer struct {
+	optimizedFSM FSM
+	semanticFSM  *semantic.FSM
+}
+
+func New() *Optimizer {
+	return &Optimizer{}
+}
+
+func (o *Optimizer) Optimize(fsm *semantic.FSM) FSM {
+	o.optimizedFSM = FSM{}
+	o.semanticFSM = fsm
+
+	o.optimizedFSM.Name = o.semanticFSM.Name
+	o.optimizedFSM.ActionsClass = o.semanticFSM.ActionsClass
+	o.optimizedFSM.InitialState = o.semanticFSM.InitialState.Name
+
+	for _, s := range o.semanticFSM.States {
+		state := State{Name: s.Name}
+
+		for _, t := range s.Transitions {
+			transition := Transition{Event: t.Event, NextState: t.NextState.Name, Actions: t.Actions}
+			state.Transitions = append(state.Transitions, transition)
+		}
+
+		o.optimizedFSM.States = append(o.optimizedFSM.States, state)
+	}
+
+	return o.optimizedFSM
+}
--- internal/smc/optimizer/optimizer_test.go
+++ internal/smc/optimizer/optimizer_test.go
@@ -1,1 +1,55 @@
 package optimizer
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/geisonbiazus/smc/internal/smc/lexer"
+	"github.com/geisonbiazus/smc/internal/smc/parser"
+	"github.com/geisonbiazus/smc/internal/smc/semantic"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOptimizer(t *testing.T) {
+	t.Run("Simple FSM", func(t *testing.T) {
+		optimizedFSM := optimizeFSM(`
+      FSM: a
+      Actions: b
+      Initial: c
+      {
+        d e f g
+        h i j {k l}
+      }
+    `)
+
+		assert.Equal(t,
+			FSM{
+				Name:         "a",
+				ActionsClass: "b",
+				InitialState: "c",
+				States: []State{
+					{Name: "d", Transitions: []Transition{
+						{Event: "e", NextState: "f", Actions: []string{"g"}},
+					}},
+					{Name: "h", Transitions: []Transition{
+						{Event: "i", NextState: "j", Actions: []string{"k", "l"}},
+					}},
+				},
+			},
+			optimizedFSM,
+		)
+	})
+}
+
+func optimizeFSM(input string) FSM {
+	builder := parser.NewSyntaxBuilder()
+	parser := parser.NewParser(builder)
+	lexer := lexer.NewLexer(parser)
+	lexer.Lex(bytes.NewBufferString(input))
+
+	analyzer := semantic.NewAnalyzer()
+	semanticFSM := analyzer.Analyze(builder.FSM())
+	opt := New()
+
+	return opt.Optimize(semanticFSM)
+}
